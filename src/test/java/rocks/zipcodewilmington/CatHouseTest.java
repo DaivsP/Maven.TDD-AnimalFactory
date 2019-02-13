334e62a3ae4843fe8905f015e71d31896f67e118
Davis Perchik <davisperchik@zipcoders-MacBook-Pro.local>
2019-02-13 17:18:45 -0500

    AnimalFactoryTest works and starting CatHouseTest

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,5 +1,11 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Before;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
@@ -9,4 +15,20 @@ public class CatHouseTest {
     // TODO - Create tests for `void remove(Cat cat)`
     // TODO - Create tests for `Cat getCatById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfCats()`
+
+    private Cat cat;
+
+    @Before
+    public void setup() {
+        this.cat = new Cat("Davis", new Date(25), 45);
+    }
+
+    @Test
+    public void addTest(){
+        //Given
+        //Cat already exists
+
+        //When
+
+    }
 }
